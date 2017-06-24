fd073ec9248f7f4e206361e47b6107ecfe0c3685
joao.chitas <joao.chitas@syone.com>
2017-06-24 22:53:23 +0100

    added view model

--- HotFix/HotFix/Models/HousingModel.cs
+++ HotFix/HotFix/Models/HousingModel.cs
@@ -7,9 +7,29 @@ namespace HotFix.Models
 {
     public class HousingModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
+        public string Description { get; set; }
+        public int Rooms { get; set; }
         public AddressModel Address { get; set; }
+        public UserModel CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class HousingViewModel
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public int Rooms { get; set; }
+
+        //for address information
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+
         public UserModel CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
     }
