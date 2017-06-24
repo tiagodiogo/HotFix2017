9196cf2174c4f9106d3c82823ac895832507fae5
Recio <recio1991@gmail.com>
2017-06-25 00:16:05 +0100

    Uma batata para o chitas

---
+++ HotFix/HotFix/Models/GMapsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotFix.Models
+{
+    public class GMapsModel
+    {
+        public string Query { get; set; }
+    }
+}
