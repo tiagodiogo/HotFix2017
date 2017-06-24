dc326531a9c74ebdfabe9c8cbc6f5a5948d5d251
joao.chitas <joao.chitas@syone.com>
2017-06-24 17:32:35 +0100

    created event page, event service for storing and information regarding events

--- HotFix/HotFix/Controllers/EventsController.cs
+++ HotFix/HotFix/Controllers/EventsController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HotFix.Models;
+using HotFix.Services;
 
 namespace HotFix.Controllers
 {
@@ -11,11 +13,26 @@ namespace HotFix.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var events = EventsService.GetInstance().GetEvents();
+            return View(events);
         }
 
-        public ActionResult AddEvent(EventModel model) {
-            return View();
+        public ActionResult Create() 
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult AddEvent(EventViewModel model)
+        {
+            var user = UserService.GetInstance().GetUser();
+            model.CreatedBy = user;
+            model.CreatedAt = DateTime.Now;
+            model.Time = DateTime.Now;
+
+            EventsService.GetInstance().CreateEvent(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- HotFix/HotFix/Models/EventModel.cs
+++ HotFix/HotFix/Models/EventModel.cs
@@ -9,7 +9,27 @@ namespace HotFix.Models
     {
         public string Name { get; set; }
         public string Category { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
         public AddressModel Address { get; set; }
+        public DateTime Time { get; set; }
+        public UserModel CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class EventViewModel
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        // for the address model
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+
         public DateTime Time { get; set; }
         public UserModel CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
